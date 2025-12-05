f8d9c823c1e76c61b7e2697803365093cd7fab59
jengaeafrica24-ship-it <kelvinwepo7710@gmail.com>
2025-12-05 14:43:40 +0300

    chore: Remove old frontend folder (replaced by frontend with space)

--- frontend/src/hooks/index.js
+++
@@ -1,3 +0,0 @@
-export { default as useSpeechRecognition } from './useSpeechRecognition';
-export { default as useTextToSpeech } from './useTextToSpeech';
-export { default as useKeyboardNavigation } from './useKeyboardNavigation';
